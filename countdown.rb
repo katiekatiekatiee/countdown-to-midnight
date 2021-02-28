0f7693a002853173b947cdcf51ccd73e220acd48
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-02-28 17:59:34 -0500

    commit tree 4856844dedf02934d7d87bdd86558fcdfeb5bb8a

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+number = 10
 
-def countdown
+def countdown_with_sleep(num_secs)
+    sleep(5)
+end
+
+def countdown(number)
+    while number <= 10 
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        if number == 0
+            break
+        end
+    end
+    "HAPPY NEW YEAR!"
 end
